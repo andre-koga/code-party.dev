3c9a640ef82a8cecdabe3bd4fc2f54dd97e6d7cc
S Taubman <akoga3@gatech.edu>
2024-09-17 17:43:03 -0400

    first commit of svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let test = "Hello World";
+</script>
+
+<h1 class="bg-slate-50 dark:bg-red-400">Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
